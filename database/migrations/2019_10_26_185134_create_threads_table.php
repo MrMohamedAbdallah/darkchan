47c5c8bc695ef5fefdcdbaeaa8b473e9b85f745a
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2019-10-27 00:21:19 +0200

    Added migration

---
+++ database/migrations/2019_10_26_185134_create_threads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text("title");
+            $table->string("name");
+            $table->text("content");
+            $table->string("file");
+            $table->string("password");
+            $table->unsignedBigInteger("board_id");
+            $table->foreign("board_id")->references("id")->on("boards")->onDelete("cascade")->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
---
+++ database/migrations/2019_10_26_185444_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("name");
+            $table->text("content");
+            $table->string("file");
+            $table->string("password");
+            $table->unsignedBigInteger("thread_id");
+            $table->foreign("thread_id")->references("id")->on("threads")->onDelete("cascade")->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
